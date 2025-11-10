f5f97dceb03fbdc647dcde7646c1f02dbda46ebf
SomorMK <samormk6@gmail.com>
2025-11-10 11:20:21 +0600

    Logout feature addade for desktop

--- src/components/common/SecondaryBtn.jsx
+++ src/components/common/SecondaryBtn.jsx
@@ -1,14 +1,29 @@
-import React from "react";
 import { Link } from "react-router";
 
-export default function SecondaryBtn({ text, to, className = "" }) {
+export default function SecondaryBtn({
+  text,
+  to,
+  className = "",
+  onClick,
+  varient,
+}) {
   return (
-    <Link to={to}>
-      <button
-        className={`rounded-full border px-4 py-3 border-primary hover:bg-secondary cursor-pointer duration-300 text-primary font-semibold ${className}`}
-      >
-        {text}
-      </button>
-    </Link>
+    <>
+      {varient == "button" ? (
+        <button
+          onClick={onClick}
+          className={`rounded-full border px-4 py-3 border-primary hover:bg-secondary cursor-pointer duration-300 text-primary font-semibold ${className}`}
+        >
+          {text}
+        </button>
+      ) : (
+        <Link
+          to={to}
+          className={`rounded-full border px-4 py-3 border-primary hover:bg-secondary cursor-pointer duration-300 text-primary font-semibold ${className}`}
+        >
+          {text}
+        </Link>
+      )}
+    </>
   );
 }
